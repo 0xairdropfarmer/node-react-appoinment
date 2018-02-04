74de503392112b1255b9ff80e0d629348c1e21cf
Kohwo Orien <kels-orien@users.noreply.github.com>
2018-02-04 13:15:57 +0100

    Update appointments.js

--- api/controllers/appointments.js
+++ api/controllers/appointments.js
@@ -1,42 +1,4 @@
 
-var path = require("path");
-var favicon = require("serve-favicon");
-var logger = require("morgan");
-var cookieParser = require("cookie-parser");
-var bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-
-var index = require("./routes/index");
-const api = require("./routes/api/index");
-
-var app = express();
-
-mongoose.Promise = global.Promise;
-
-//Adds connection to database using mongoose
-//for <dbuser>:replace with your username, <dbpassword>: replace with your password.
-//<DATABASE_URL>: replace with database url, example:ds234562.mlab.com:17283
-mongoose.connect("<dbuser>:<dbpassword>@<DATABASE_URL>/appointments", {
-  useMongoClient: true
-});
-
-//This enabled CORS, Cross-origin resource sharing (CORS) is a mechanism that allows restricted resources (e.g. fonts)
-//on a web page to be requested from another domain outside the domain from which the first resource was served
-
-app.all("/*", function(req, res, next) {
-  // CORS headers
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  // Set custom headers for CORS
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-type,Accept,X-Access-Token,X-Key"
-  );
-  if (req.method == "OPTIONS") {
-    res.status(200).end();
-  } else {
-    next();
-var Model = require("../models/index");
 const { Appointment, Slot } = Model;
 const Nexmo = require("nexmo");
 
@@ -94,41 +56,7 @@ const appointmentController = {
         }
       });
     });
-
   }
-});
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+};
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", index);
-app.use("/api", api);
-//app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-module.exports = app;
+module.exports = appointmentController;
